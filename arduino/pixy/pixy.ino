a36d68fd705a900337b1e6f1dff32968123a705a
Thomas <19thomastrimble@gmail.com>
2021-07-22 19:45:42 -0700

    Fixed distance estimation, lighting bad :(

--- arduino/pixy/pixy.ino
+++ arduino/pixy/pixy.ino
@@ -1,9 +1,16 @@
 #include <Pixy2.h>
-const int kFocal = (35 * 19) / 9.5;
+#include <Pixy2CCC.h>
+
+//Focal Point Formula
+const int kFocal = (106 * 20) / 9.25;
 
 Pixy2 pixy;
 void setup() {
+
+  //baud num
   Serial.begin(115200);
+
+  //start print
   Serial.print("Woah its working\n");
 
   pixy.init();
@@ -12,9 +19,21 @@ void setup() {
 void loop() {
   
   int8_t code = pixy.ccc.getBlocks(true, 0b1, 1);
-
+  //Serial.print("Woah its a start\n");
+  
   if (pixy.ccc.numBlocks > 0) {
     Block target = pixy.ccc.blocks[0];
-    Serial.println(9.5 * kFocal / target.m_width);
+
+    //Block detected print
+    Serial.print("Woah we detected\n");
+    
+    //Distance Formula
+    Serial.println(9.25 * kFocal / target.m_width);
+
+    //Debugging
+//    Serial.print("Width:");
+//    Serial.println(target.m_width);
+//    Serial.print("Height:");
+//    Serial.println(target.m_height);
+    }
   }
-}
